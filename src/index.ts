be7bbf74ff7d14cb337bc7313804c3586969d4ae
Danyaal Rangwala <thedanyaal@gmail.com>
2016-07-06 14:10:10 -0700

    update popgun v0.0.18 and update API methods

--- src/index.ts
+++ src/index.ts
@@ -19,28 +19,46 @@ export const angularModule = angular.module(name, [])
           }, false);
         },
 
+        // Store a group w/ options to reuse
+        // schema is a base set of options, options attr will precedence over schema
         registerGroup: function(groupId: string, opts: any): void {
           popgun.registerGroup(groupId, opts);
         },
 
+        // Store a schema of options
         registerSchema: function(schemaId: string, opts: any): void {
           popgun.registerSchema(schemaId, opts);
         },
 
+        // Returns a popover model based on a group
         getPopFromGroupId: function(groupId: string): any {
           return popgun.getPopFromGroupId(groupId);
         },
 
+        // Returns the state of a given popover
+        // (hidden, content_setup, pre_position, pre_show, showing, pre_hide)
         getPopState: function(groupId: string): string {
           return popgun.getPopState(popgun.getPopFromGroupId(groupId));
         },
 
+        // Returns a boolean about whether a popover for a specific target is alrady open
+        isPopAlreadyOpenForTarget: function(target: Element): boolean {
+          return popgun.isPopAlreadyOpenForTarget(target);
+        },
+
+        // Returns a boolean about whether a popover is already open for a group
+        isPopAlreadyOpenForGroup: function(groupId: string): boolean {
+          return popgun.isPopAlreadyOpenForGroup(groupId);
+        },
+
+        // Show the popover for a particular target
         showPop: function(target: Element, isPinned: boolean, trigger: string): void {
           return popgun.showPop(target, isPinned, trigger);
         },
 
-        hidePop: function(target: Element): void {
-          return popgun.hidePop(target);
+        // Hide the popover for a particular target
+        hidePop: function(target: Element, hideFullChain: boolean): void {
+          return popgun.hidePop(target, hideFullChain);
         }
     };
 
